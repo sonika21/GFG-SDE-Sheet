2e5af919fe181fea206835eb1f17838ec4672cbb
Sonika Panigrahi <sonika.panigrahi@gmail.com>
2022-08-03 00:13:35 +0530

    Create MinimizeSumOfProduct.cpp

---
+++ MinimizeSumOfProduct.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    long long int minValue(int a[], int b[], int n)
+    {
+       sort(a,a+n);
+        sort(b,b+n);
+        reverse(b,b+n);
+        
+        long long int  product=1;
+        long long int sum=0;
+        
+        for( long long int i=0;i<n;i++){
+             product=a[i]*b[i];
+             sum = sum+product;
+           
+        }
+        return sum;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         int n, i;
+         cin>>n;
+         int a[n], b[n];
+         for(i=0;i<n;i++)
+         cin>>a[i];
+         for(i=0;i<n;i++)
+         cin>>b[i];
+         Solution ob;
+         cout<< ob.minValue(a, b, n) <<endl;
+     }
+	
+	return 0;
+}  // } Driver Code Ends
